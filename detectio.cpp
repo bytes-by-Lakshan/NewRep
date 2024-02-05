96eae1b1bf9fce64bcb7781b0126511d86a792ad
lakshan damu <lakshandamu2904@outlook.com>
2024-02-05 21:29:08 +0530

    made som oop changes

--- detectio.cpp
+++ detectio.cpp
@@ -1,24 +1,17 @@
 #include "opencv2\highgui.hpp"
 #include "opencv2\highgui.hpp"
 
+#include <iostream>
+
+using namespace std;
+
+class capture   //Creating a class to recieve and detect objects in a video
+{
+
+
+};
+
 int main()
 {
-	//creating video capturing option
-
-	cv::VideoCapture video(0);
-
-	if (!video.isOpened()) return -1;
-
-	cv::Mat frame; //array of class to store frames
-
-
-	while (video.read(frame)) //executes till the videos last frame
-	{
-		cv::imshow("video feed", frame); // displays the frame
-
-		if (cv::waitKey(25) >= 0) break; // ends the video early if any key is pressed on the keyboard within the 25 ms delay
-	}
-
-
-		return  0;
+	
 }
